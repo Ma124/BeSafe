fb779a8da9582deb720be98a4d987318c46c35ce
Ma_124 <ma_124@outlook.com>
2018-03-26 13:36:43 +0200

    .

--- coffeescript/hash.js
+++ coffeescript/hash.js
@@ -7,8 +7,6 @@
     md.update(v);
     out = md.digest();
     document.getElementById("hex").value = out.toHex();
-    console.log(forge.util.encode64(out));
-    console.log(forge.util.decode64(out));
     return document.getElementById("b64").value = out;
   };
 
